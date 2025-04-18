4d93f7f0f8652aae8b655a6e3e427fc2932e88d3
NehaA990 <neharora990@gmail.com>
2025-04-18 13:56:48 -0700

    PE02 Submission

--- PE01/Hello-World-app/app/(tabs)/index.tsx
+++ PE01/Hello-World-app/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+// Main component that renders personal info
 export default function HelloWorldApp() {
   return (
     <View style={styles.container}>
@@ -15,8 +15,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFEB3B', // yellow background 
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'center', // Center items vertically
+    alignItems: 'center', // Dark gray text
   },
   infoText: {
     fontSize: 20,
